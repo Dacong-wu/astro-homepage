dd84cbe6a6608c616d4a2411f7a1418e0aa6fc39
Dacong-wu <2420390517@qq.com>
2024-02-02 13:31:46 +0800

    v0.0.1

--- src/components/FAQ.jsx
+++ src/components/FAQ.jsx
@@ -4,22 +4,19 @@ import { motion } from 'framer-motion'
 const FAQData = [
   {
     question: 'Spans 是什么？',
-    answer: ''
+    answer: 'Span 是一个 html 标签，这就是 Spans 的由来，没有特殊的意义，就是单纯的喜欢'
   },
   {
-    question: 'How to claim your 25% discount offer?',
-    answer:
-      'To claim your 25% discount, simply sign up for an account and enter the promotional code at checkout. The discount will be applied automatically to your purchase.'
+    question: '这个 Logo 为什么看着有点熟悉？',
+    answer: '是的，这个 Logo 的灵感来自于 onwidget'
   },
   {
-    question: "What's your refund policy?",
-    answer:
-      "We offer a 30-day money-back guarantee on all our plans. If you're not satisfied with our product, simply contact our support team within 30 days of purchase for a full refund."
+    question: '这个网站是你自己设计的吗？',
+    answer: '这个网站主题来自 Tailcast'
   },
   {
-    question: 'How to get support for the product?',
-    answer:
-      "Our dedicated support team is here to help. You can reach out to us through the contact form on our website, send an email, or engage with us via live chat. We'll be happy to assist you with any questions or concerns you may have"
+    question: '我该怎么与你取得联系？',
+    answer: '你可以给我的邮箱 dacong@ll1025.cn 发送邮件'
   }
 ]
 
