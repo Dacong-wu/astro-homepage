071c8b287379a98a1ca64244e7d2c67f78b68fe6
Dacong-wu <2420390517@qq.com>
2024-06-20 17:28:18 +0800

    use theme by astros

--- src/components/Footer.jsx
+++
@@ -1,9 +0,0 @@
-export const Footer = () => {
-  return (
-    <footer>
-      <div className=" bg-blue-600 radius-for-skewed ">
-        <p className="text-center text-sm text-gray-100 py-6 block">&copy; 2024 spans. 保留所有权利。</p>
-      </div>
-    </footer>
-  )
-}
